431232ca6e5ff874d43766efdf3a5c76a1496032
yeuchi@gmail.com <yeuchi@gmail.com>
2018-02-14 18:58:27 -0600

    Lesson 8-11

--- app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
+++ app/src/androidTest/java/com/example/android/teatime/OrderSummaryActivityTest.java
@@ -1,5 +1,8 @@
 package com.example.android.teatime;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Rule;
@@ -16,12 +19,18 @@ import static android.support.test.espresso.intent.matcher.ComponentNameMatchers
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasAction;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static android.support.test.espresso.intent.matcher.IntentMatchers.hasExtra;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.isInternal;
 import static android.support.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.core.AllOf.allOf;
 
 import android.support.test.espresso.intent.rule.IntentsTestRule;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 
 /**
  * Created by ctyeung on 2/14/18.
@@ -39,18 +48,26 @@ public class OrderSummaryActivityTest
 
     // TODO (3) Finish this method which runs before each test and will stub all external
     // intents so all external intents will be blocked
-
+    @Before
     public void stubAllExternalIntents()
     {
-
+        intending(not(isInternal()))
+                .respondWith(new Instrumentation.ActivityResult(Activity.RESULT_OK, null));
     }
 
 
     // TODO (4) Finish this method which verifies that the intent sent by clicking the send email
     // button matches the intent sent by the application
-
+    @Test
     public void clickSendEmailButton_SendsEmail()
     {
+     //   onView(withId(R.id.summary_email_prompt))
+     //           .perform(typeText("Hello world"), closeSoftKeyboard());
 
+        onView(withId(R.id.send_email_button))
+                .perform(click());
+
+        intended(allOf(hasAction(Intent.ACTION_SENDTO),
+                hasExtra(Intent.EXTRA_TEXT, "I just ordered a delicious tea from TeaTime. Next time you are craving a tea, check them out!")));
     }
 }
